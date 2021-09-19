903ed9afdf901d6d7ce058e23f0453c7b281951d
Ong Wen Kai <ongwenkai92@gmail.com>
2021-09-19 21:46:32 +0800

    database encryption

---
+++ app.js
@@ -0,0 +1,65 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+mongoose.connect("mongodb://localhost:27017/UserDatabase");
+ const UserSchema = new mongoose.Schema({
+   email:String,
+   password:String
+ });
+
+
+var encKey = "Thisisourlittlesecret";
+var sigKey = "Mynameiswenkai";
+UserSchema.plugin(encrypt, { secret:process.env.SECRET,encryptedFields:["password"] });
+const User  = new mongoose.model("user",UserSchema);
+
+const app = express();
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+  res.render("home");
+});
+app.get("/login",(req,res)=>{
+  res.render("login");
+});
+app.post("/login",function(req,res){
+  User.findOne({
+    email:req.body.username,
+
+  },function(err,foundUser){
+    if(err)console.log(err);
+    else
+    {
+      if(foundUser)
+      {
+        if(foundUser.password === req.body.password)res.render("secrets");
+      }
+      else res.send("Fail to login");
+    }
+  });
+});
+app.get("/register",(req,res)=>{
+  res.render("register");
+});
+
+app.post("/register",function(req,res){
+  const email=req.body.username;
+  const password = req.body.password;
+  const newUser = new User({
+    email:email,
+    password:password
+  });
+  newUser.save(function(err){
+    if(err)res.send(err);
+    else res.render("secrets");
+  });
+});
+app.listen(3000,function(){
+  console.log("listening on port 3000...");
+});
